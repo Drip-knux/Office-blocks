ceb620b371f1f99f3ff8acb4245e23b61d8ab42c
LminusSucksAtCoding <lminusmt@gmail.com>
2024-08-28 14:33:19 -0500

    take 2, fix directory issues

---
+++ init.lua
@@ -0,0 +1,13 @@
+print("This file will be run at load time!")
+
+dofile(minetest.get_modpath("retro_office") .. "/walls.lua")
+
+dofile(minetest.get_modpath("retro_office") .. "/lights.lua")
+
+dofile(minetest.get_modpath("retro_office") .. "/tile.lua")
+
+dofile(minetest.get_modpath("retro_office") .. "/wools.lua")
+
+--dofile(minetest.get_modpath("retro_office") .. "/electronics.lua")
+
+--dofile(minetest.get_modpath("retro_office") .. "/pneumatics.lua")
---
+++ wools.lua
@@ -0,0 +1,57 @@
+--this file contains the wools--
+
+--white--
+minetest.register_node("retro_office:white_wool", {
+    description = "white wool",
+    tiles = {"white_wool.png"},
+    groups = {cracky=3, stone=1}
+})
+
+--yellow--
+minetest.register_node("retro_office:yellow_wool", {
+    description = "yellow wool",
+    tiles = {"yellow_wool.png"},
+    groups = {cracky=3, stone=1}
+})
+
+--orange--
+minetest.register_node("retro_office:orange_wool", {
+    description = "orange wool",
+    tiles = {"orange_wool.png"},
+    groups = {cracky=3, stone=1}
+})
+
+--red--
+minetest.register_node("retro_office:red_wool", {
+    description = "red wool",
+    tiles = {"red_wool.png"},
+    groups = {cracky=3, stone=1}
+})
+
+--green--
+minetest.register_node("retro_office:green_wool", {
+    description = "green wool",
+    tiles	= {"green_wool.png"},
+    groups = {cracky=3, stone=1}
+})
+
+--light green--
+minetest.register_node("retro_office:light_green_wool", {
+    description = "light green_wool",
+    tiles = {"light_green_wool.png"},
+    groups = {cracky=3, stone=1}
+})
+
+--light blue--
+minetest.register_node("retro_office:light_blue_wool", {
+    description = "light_blue_wool",
+    tiles = {"light_blue_wool.png"},
+    groups = {cracky=3, stone=1}
+})
+
+--blue--
+minetest.register_node("retro_office:blue_wool", {
+    description = "blue_wool",
+    tiles = {"blue_wool.png"},
+    groups = {cracky=3, stone=1}
+})

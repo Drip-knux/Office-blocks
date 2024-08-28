4d66649e67f6d94cba4fbf3d11d496e894c3ffc6
Drip-knux <168502501+Drip-knux@users.noreply.github.com>
2024-08-28 13:28:15 -0600

    Initial Commit
    Uploading the whole base of the mod

---
+++ retro_office/tile.lua
@@ -0,0 +1,57 @@
+--this file contains the tiles--
+
+--white--
+minetest.register_node("retro_office:white_tile", {
+    description = "white tile",
+    tiles = {"white_tile.png"},
+    groups = {cracky=3, stone=1}
+})
+
+--yellow--
+minetest.register_node("retro_office:yellow_tile", {
+    description = "yellow tile",
+    tiles = {"yellow_tile.png"},
+    groups = {cracky=3, stone=1}
+})
+
+--orange--
+minetest.register_node("retro_office:orange_tile", {
+    description = "orange tile",
+    tiles = {"orange_tile.png"},
+    groups = {cracky=3, stone=1}
+})
+
+--red--
+minetest.register_node("retro_office:red_tile", {
+    description = "red tile",
+    tiles = {"red_tile.png"},
+    groups = {cracky=3, stone=1}
+})
+
+--green--
+minetest.register_node("retro_office:green_tile", {
+    description = "green tile",
+    tiles = {"green_tile.png"},
+    groups = {cracky=3, stone=1}
+})
+
+--light green--
+minetest.register_node("retro_office:light_green_tile", {
+    description = "light green_tile",
+    tiles = {"light_green_tile.png"},
+    groups = {cracky=3, stone=1}
+})
+
+--light blue--
+minetest.register_node("retro_office:light_blue_tile", {
+    description = "light_blue_tile",
+    tiles = {"light_blue_tile.png"},
+    groups = {cracky=3, stone=1}
+})
+
+--blue--
+minetest.register_node("retro_office:blue_tile", {
+    description = "blue_tile",
+    tiles = {"blue_tile.png"},
+    groups = {cracky=3, stone=1}
+})
